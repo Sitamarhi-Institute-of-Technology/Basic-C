567757769051fdea8bd9dfa364c26d42953a703e
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-02 18:17:40 +0000

    Jun 2, 2023, 11:47 PM

--- basic_output.c
+++ basic_output.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main()
 {
+    //this is single comment line
+
+    /*
+    this
+    is
+    multi commnet line
+    */
+
+   // How to print Strings ?
+
     printf("Hello GitHub Community");
+    printf("I was born on 1 july, Not really JK !");
+
     
 }
