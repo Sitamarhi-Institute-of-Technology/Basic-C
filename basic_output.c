3a997e6675c4a488c3f8930d3b4ad2964bcfb867
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-02 18:23:10 +0000

    Jun 2, 2023, 11:53 PM

--- basic_output.c
+++ basic_output.c
@@ -11,7 +11,10 @@ int main()
 
    // How to print Strings ?
 
+    // here all the characters are counted as String (Strings: they are just symbols we can't apply maths on them.)
     printf("Hello GitHub Community");
+
+    // here the number i used here 1 is also count as a string and the symbols ( , and !) are also string (they are useless symbols just used display message)
     printf("I was born on 1 july, Not really JK !");
 
     
