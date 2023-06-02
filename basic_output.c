df3c83a205d9fa1dabd3ad7bcf08a20b0ed82216
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-02 18:42:05 +0000

    Jun 3, 2023, 12:12 AM

--- basic_output.c
+++ basic_output.c
@@ -9,7 +9,7 @@ int main()
     multi commnet line
     */
 
-   //# HOW TO PRINT STRINGS ?
+    //# HOW TO PRINT STRINGS ?
 
         //## HERE ALL THE CHARACTERS ARE COUNTED AS STRING (STRINGS: THEY ARE JUST SYMBOLS WE CAN'T APPLY MATHS ON THEM.)
         printf("Hello GitHub Community.");
@@ -17,5 +17,8 @@ int main()
         //## HERE THE NUMBER I USED HERE 1 IS ALSO COUNT AS A STRING AND THE SYMBOLS ( , AND !) ARE ALSO STRING (THEY ARE USELESS SYMBOLS JUST USED TO DISPLAY MESSAGE)
         printf("I was born on 1 july, Not really JK !");
 
+    //# HOW TO PRINT Messages in Multi line ?
+
+        //## if you had runned the above code it seem like
     
 }
