71beae98c41ec9458e978cd55bbdb4651f1f542a
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-03 16:30:12 +0000

    Jun 3, 2023, 10:00 PM

--- 2_printing_shapes_using_printf.c
+++ 2_printing_shapes_using_printf.c
@@ -7,10 +7,10 @@ int main()
     printf("   *****\n");
     printf("  *******\n");
     printf(" *********\n");
-    printf("    |||");
-    printf("    |||");
-    printf("    |||");
-    printf("    |||");
+    printf("    |||\n");
+    printf("    |||\n");
+    printf("    |||\n");
+    printf("    |||\n");
 
 
 }
