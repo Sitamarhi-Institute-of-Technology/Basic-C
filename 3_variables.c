8d4b052ddb5360992ca51f3b74dd8f3b95b954a5
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-06 17:54:38 +0000

    Jun 6, 2023, 11:24 PM

--- 3_variables.c
+++ 3_variables.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
 int main()
 {
    //# VARIABLES ARE LIKE BOXES WHERE YOU CAN PUT YOUR THINGS
@@ -40,6 +44,24 @@ int main()
    printf("Size of float: %zu bytes\n", sizeof(floatType));
    printf("Size of double: %zu bytes\n", sizeof(doubleType));
    printf("Size of char: %zu byte\n", sizeof(charType));
+
+   printf( "Ranges for integer data types in C\n\n" );
+   printf( "int8_t    %20d  %20d\n"     , SCHAR_MIN , SCHAR_MAX  );
+   printf( "------------------------------------------------------------\n");
+   printf( "int16_t   %20d  %20d\n"     , SHRT_MIN  , SHRT_MAX   );
+   printf( "int32_t   %20d  %20d\n"     , INT_MIN   , INT_MAX    );
+   printf( "int64_t   %20lld  %20lld\n" , LLONG_MIN , LLONG_MAX  );
+   printf( "uint8_t   %20d  %20d\n"     , 0         , UCHAR_MAX  );
+   printf( "uint16_t  %20d  %20d\n"     , 0         , USHRT_MAX  );
+   printf( "uint32_t  %20d  %20u\n"     , 0         , UINT_MAX   );
+   printf( "uint64_t  %20d  %20llu\n"   , 0         , ULLONG_MAX );
+   printf( "\n" );
+   printf( "============================================================\n\n");
+   printf( "Ranges for real number data types in C\n\n" );
+   printf( "------------------------------------------------------------\n");
+   printf( "float        %14.7g  %14.7g\n"   , FLT_MIN  , FLT_MAX  );
+   printf( "double       %14.7g  %14.7g\n"   , DBL_MIN  , DBL_MAX  );
+   printf( "long double  %14.7Lg  %14.7Lg\n" , LDBL_MIN , LDBL_MAX );
 
 
 
