8018ec4ec452e692f05c8e094fb1ebb7dd390127
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-07 06:44:02 +0000

    Jun 7, 2023, 12:14 PM

--- 4_basic_input.c
+++ 4_basic_input.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 void main()
 {
-    // DECLARATION OF VARIABLE : WHEN WE CREATE A VARIABLE WE CALL IT DECLARATION
+    // DEFINITON OF VARIABLE : WHEN WE CREATE A VARIABLE WE CALL IT DEFINITION
     int a,b,c; // DECLARING MULTIPLE VARIABLE IN ONE LINE
 
     // INITALIZATION OF VARIABLE
+    a=10;  // WHEN ASSIGNING A VALUE TO A VARIABLE IS CALLED INITIALIZATION
+
+    printf("\n value of a = %d",a);
+
+    // ASSIGNING NEW VALUE TO "a"
+    a=20;
+    printf("\nNew assign value of a = %d",a);
+    
+
 
 
 
