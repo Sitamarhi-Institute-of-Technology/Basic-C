1c2cd07ee037a43c778928cc4ca1a251bbef7b5a
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-10 18:11:18 +0000

    Jun 10, 2023, 11:41 PM

--- 5_Arithmetic_operations.c
+++ 5_Arithmetic_operations.c
@@ -6,8 +6,13 @@ int main()
     
     double num1,num2,num3;
 
-    printf("\nENTER NUM1 = ")
+    printf("\nENTER NUM1 = ");
+    scanf("%lf",&num1);
 
+    printf("\nENTER NUM2 = ");
+    scanf("%lf",&num1);
+
+    printf("\n")
 
 
 
