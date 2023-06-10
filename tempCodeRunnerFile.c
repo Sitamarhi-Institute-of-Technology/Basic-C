a7c9418670c4f35790e977dbeaae90fb3be0b704
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-10 16:55:28 +0000

    Jun 10, 2023, 10:25 PM

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+   // printf("\nEnter Your First name = ");
+    // scanf("%s",&Fname); // GETTING INPUT FROM THE USER HERE "%s" IS FORMAT SPECIFIER FOR WORDS WHOSE LENGTH GRATER THAN 1 AND "&" IS AN ATTRIBUTE THAT ACCESS THE MEMORY LOCATION OF "Fname" VARIABLE
+    // printf("\nEnter Your First name = ");
+    // scanf("%s",&Lname);
+    // printf("\nEnter Your age = ");
+    // scanf("%d,&age");
+    // printf("\nEnter Your salary = ");
+    // scanf("%lf",&income);
+
+    // printf("\n++++++++++++++++++++++++++++++++++++++++++++");
+    // printf("\n++ First name =           %15s ++",Fname);
+    // printf("\n++ Last name  =           %15s ++",Lname);
+    // printf("\n++ age        =           %15d ++",age);
+    // printf("\n++ Income     =          %16lf ++",income);
+    // printf("\n++++++++++++++++++++++++++++++++++++++++++++");
