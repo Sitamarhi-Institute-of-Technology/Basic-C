2d701f3ca7caad27efb598175a63f8f94a486ef8
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-18 13:27:03 +0000

    Jun 18, 2023, 6:57 PM

--- 7_bitwise-operation.c
+++ 7_bitwise-operation.c
@@ -17,7 +17,11 @@ int main() {
     printf("Output = %d\n", ~-12);
 
     // RIGHT SHIFT OPERATOR (RIGHT SHIFT OPERATOR SHIFTS ALL BITS TOWARDS RIGHT BY CERTAIN NUMBER OF SPECIFIED BITS. IT IS DENOTED BY >>)
-    
+    int num=212, i;
+
+    for (i = 0; i <= 2; ++i) {
+        printf("Right shift by %d: %d\n", i, num >> i);
+    }
 
     return 0;
 }
