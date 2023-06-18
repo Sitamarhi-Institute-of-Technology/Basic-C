0728678a6746f429d5c421a2e2a68aaf416a8dbd
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-18 13:28:27 +0000

    Jun 18, 2023, 6:58 PM

--- 7_bitwise-operation.c
+++ 7_bitwise-operation.c
@@ -4,17 +4,17 @@ int main() {
 
     // BITWISE AND OPERATOR &
     int a = 12, b = 25;
-    printf("Output = %d", a & b);
+    printf("\nOutput = %d", a & b);
 
     // BITWISE OR OPERATOR |
-    printf("Output = %d", a | b);
+    printf("\nOutput = %d", a | b);
 
     // BITWISE XOR (EXCLUSIVE OR) OPERATOR ^
-    printf("Output = %d", a ^ b);
+    printf("\nOutput = %d", a ^ b);
 
     // BITWISE COMPLEMENT OPERATOR ~
-    printf("Output = %d\n", ~35);
-    printf("Output = %d\n", ~-12);
+    printf("\nOutput = %d\n", ~35);
+    printf("\nOutput = %d\n", ~-12);
 
     // RIGHT SHIFT OPERATOR (RIGHT SHIFT OPERATOR SHIFTS ALL BITS TOWARDS RIGHT BY CERTAIN NUMBER OF SPECIFIED BITS. IT IS DENOTED BY >>)
     int num=212, i;
@@ -24,6 +24,10 @@ int main() {
     }
 
     // LEFT SHIFT OPERATOR (LEFT SHIFT OPERATOR SHIFTS ALL BITS TOWARDS LEFT BY A CERTAIN NUMBER OF SPECIFIED BITS. THE BIT POSITIONS THAT HAVE BEEN VACATED BY THE LEFT SHIFT OPERATOR ARE FILLED WITH 0. THE SYMBOL OF THE LEFT SHIFT OPERATOR IS <<)
-    
+    printf("\n");
+
+    for (i = 0; i <= 2; ++i) {
+        printf("Left shift by %d: %d\n", i, num << i);    
+    }
     return 0;
 }
