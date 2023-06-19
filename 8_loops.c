5e0119b0630832537bdc26de63de310a1ea8e9f2
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-19 03:11:34 +0000

    Jun 19, 2023, 8:41 AM

---
+++ 8_loops.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    // LOOPING IS DOING A TASK AGAIN AND AGAIN IN A CYCLE UNTIL A CERTAIN PARAMETER IS SATISFIED
+    // EXAMPLE : PRINT 0 TO 10 (WE WILL DO IT BY TO APPROACH)
+    printf("\nprinting by approach 1: 1 2 3 4 5 6 7 8 9 10");
+}
