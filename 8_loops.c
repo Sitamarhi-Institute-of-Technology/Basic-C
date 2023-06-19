c2e098d9d8715c9c83b98c9c1aae4e26734e8235
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-19 03:15:01 +0000

    Jun 19, 2023, 8:45 AM

--- 8_loops.c
+++ 8_loops.c
@@ -6,7 +6,7 @@ int main()
     printf("\nprinting by approach 1: 1 2 3 4 5 6 7 8 9 10");
     
     int i=0;
-    printf("\nprintfing by approach 2:");
+    printf("\nprinting by approach 2:");
     while(i<=10)
     {
         printf("%d",i);
