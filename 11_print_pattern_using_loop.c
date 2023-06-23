4282a7de4d2031af4453a5dc4c78def719bdecda
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-23 19:39:39 +0000

    Jun 24, 2023, 1:09 AM

--- 11_print_pattern_using_loop.c
+++ 11_print_pattern_using_loop.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int rows, i, j;
 
     printf("Enter the number of rows: ");
@@ -12,6 +13,14 @@ int main() {
         }
         printf("\n");
     }
+
+    /*
+    Here, we take input from the user for the number of rows in the triangle. Then, we use two loops - the outer loop to iterate over the rows, and the inner loop to print the asterisks for each row.
+
+    For each row, we print i number of asterisks (starting from 1 and going up to the current row number). Then, we print a newline character to move to the next row.
+
+    This will print a triangle pattern made of asterisks, with the number of rows specified by the user. You can modify this pattern or try creating your own patterns using loops in C.
+    */
 
     return 0;
 }
