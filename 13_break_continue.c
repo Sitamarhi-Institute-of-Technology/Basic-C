b946f98b0ea10037f387c10c9eed4a470c975190
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-26 01:18:40 +0000

    Jun 26, 2023, 6:48 AM

--- 13_break_continue.c
+++ 13_break_continue.c
@@ -22,3 +22,7 @@ int main() {
 
     return 0;
 }
+
+/*In this code, we have used two loops: for loop to iterate over the numbers 1 to 10. Inside the first loop, we have used the if statement to check if i is equal to 5. If it is, we use the break statement to exit the loop. This means that the loop will stop executing once it reaches i = 5.
+
+After that, we have used another for loop to iterate over the numbers 1 to 10 again. Inside this loop, we have used the if statement to check if i is even. If it is, we use the continue statement to skip that iteration and move on to the next one. This means that the loop will not print any even numbers.*/
