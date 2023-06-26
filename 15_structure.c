552d3904674d330058859af93b1ab62283afe062
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-06-26 01:24:32 +0000

    Jun 26, 2023, 6:54 AM

---
+++ 15_structure.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct student {
+   char name[50];
+   int roll;
+   float marks;
+};
+
+int main() {
+   struct student s1; // declaring a struct variable
+
+   // assigning values to the struct members
+   strcpy(s1.name, "John");
+   s1.roll = 1;
+   s1.marks = 85.6;
+
+   // displaying the values of the struct members
+   printf("Name: %s\n", s1.name);
+   printf("Roll Number: %d\n", s1.roll);
+   printf("Marks: %.2f\n", s1.marks);
+
+   return 0;
+}
